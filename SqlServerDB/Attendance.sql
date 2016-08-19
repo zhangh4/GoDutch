7b03c9d7ee2338bdf4871560761850619ea1ec45
Gary Zhang <gary.zhang@healthtrustpg.com>
2016-08-19 11:21:13 -0500

    change DB schema

--- SqlServerDB/Attendance.sql
+++ SqlServerDB/Attendance.sql
@@ -1,10 +1,10 @@
-﻿CREATE TABLE [dbo].[AttendingFamily]
+﻿CREATE TABLE [dbo].[Attendance]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [FamilyId] INT NOT NULL, 
     [ExpenseId] INT NOT NULL, 
-    [Expense] MONEY NULL, 
-    [Count] FLOAT NULL, 
+    [Cost] MONEY NULL, 
+    [HeadCount] FLOAT NULL, 
     CONSTRAINT [FK_AttendingFamily_ToFamily] FOREIGN KEY ([FamilyId]) REFERENCES [Family]([Id]), 
     CONSTRAINT [FK_AttendingFamily_ToExpense] FOREIGN KEY ([ExpenseId]) REFERENCES [Expense]([Id]) ON DELETE CASCADE
 )
